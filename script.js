fc2bd7d6bc96a2277de264ce4737b9c3019de6c9
Ioannis Tourtouras <yiannistourtce@gmail.com>
2022-02-25 15:50:32 +0100

    Fr2nd

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const startDisplay = document.querySelector('.game-intro')
 let bg
+const gameBoard = document.querySelector('#game-board')
+let shipImg = loadImage("assets/Ship1.png")
 
 
 function setup() {
@@ -7,7 +9,7 @@ function setup() {
     // into the createCanvas() method. In this program, the size of
     // the image is 720x400 pixels.
     bg = loadImage('assets/gameBoard.jpg');
-    const canvas = createCanvas(300, 200)
+    const canvas = createCanvas(1000, 900)
     canvas.parent('game-board');
     
     console.log(bg)
@@ -17,6 +19,22 @@ function setup() {
       background(bg)
   }
 
+ class Player {
+     constructor(x, y, width, height) {
+         this.x = x
+         this.y = y
+         this.width = width
+         this.height = height
+     }
+ }
+
+ class Spaceship extends Player {
+     constructor(img) {
+     super(x, y, width, height)
+     this.img = shipImg
+     }
+ } 
+
 
 
 
@@ -25,11 +43,14 @@ function setup() {
 
 
   window.onload = () => {
+
+    gameBoard.style.display = 'none'
     document.getElementById('start-button').onclick = () => {
       startGame();
     };
-  
+    
     function startGame() {    
       startDisplay.style.display = 'none'
+      gameBoard.style.display = 'block'
     }
   };
